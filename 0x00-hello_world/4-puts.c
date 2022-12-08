8e5ac2f7e8f9506dbbbef29c4694f97812b1ee39
Lonerangervin <vincentbrion8@gmail.com>
2022-12-08 09:26:49 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@
  * return: Always 0 (success)
  */int main(void)
   {
-  puts("\"Programming is like building a multilingual puzzle");
-  
+  puts("\"Programming is like building a multilingual puzzle"); 
   return 0;
   }
