b654ae1d6c098d1adb5a4119ecf5ea8eb732ce63
Lonerangervin <vincentbrion8@gmail.com>
2022-12-08 09:46:10 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,6 @@
  * *main - print a code line using puts
  * Return: 0
 */int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle"); 
+{puts("\"Programming is like building a multilingual puzzle"); 
   return (0);
 }
